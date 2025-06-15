e698a47fbfd505d2690b16e6ba8843261b3d5a47
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2025-06-15 09:52:11 +0530

    Add files via upload

--- mergesort.c
+++ mergesort.c
@@ -1,97 +1,84 @@
-/*Design and impletement c/c++ program to sort a given set of integer elements using merge sort method and compute its time complexity Run
-the program for varied values of n> 5000, and record the time taken to sort. plot a graph of time versus n.The elements can be read from a
-file or can be generated using the random number generator.*/
-//It is comes under divide and conqure method.
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include"stdio.h"
+#include"time.h"
 
-void mergesort(int*, int, int);
-void merge(int*, int, int, int);
+void mergesort(int*,int,int);
+int merge(int*,int,int,int);
 int n;
 
-void mergesort(int a[], int low, int high) {
-    if (low < high) {
-        int mid = (low + high) / 2;
-        mergesort(a, low, mid);
-        mergesort(a, mid + 1, high);
-        merge(a, low, mid, high);
-    }
+void mergesort(int a[], int low, int high)
+{
+	int mid;
+	if(low<high)
+	{
+		mid=(low+high)/2;
+		mergesort(a,low,mid);
+		mergesort(a,mid+1,high);
+		merge(a,low,mid,high);
+	}
 }
 
-void merge(int a[], int low, int mid, int high) {
-    int i = low, j = mid + 1, k = 0;
-    int size = high - low + 1;
-    int *b = (int*) malloc(size * sizeof(int));
-
-    if (!b) {
-        printf("Memory allocation failed\n");
-        return;
-    }
-
-    while (i <= mid && j <= high) {
-        if (a[i] < a[j]) {
-            b[k++] = a[i++];
-        } else {
-            b[k++] = a[j++];
-        }
-    }
-    
-    while (i <= mid) {
-        b[k++] = a[i++];
-    }
-    
-    while (j <= high) {
-        b[k++] = a[j++];
-    }
-
-    for (i = low, k = 0; i <= high; i++, k++) {
-        a[i] = b[k];
-    }
-
-    free(b);
+int merge(int a[], int low, int mid, int high)
+{
+	int i,j,h,k,*b;
+	b=(int*)malloc(n*sizeof(int));
+	h=low;
+	i=low;
+	j=mid+1;
+	while((h<=mid)&&(j<=high))
+	{
+		if(a[h]<a[j]){
+			b[i]=a[h];
+			h=h+1;
+		}else{
+			b[i]=a[j];
+			j=j+1;
+		}
+		i=i+1;
+	}
+	if(h>mid){
+		for(k=j;k<=high;k++){
+			b[j]=a[k];
+			i=i+1;
+		}
+	}
+	else{
+		for(k=h;k<=mid;k++){
+			b[i]=a[k];
+			i=i+1;
+		}
+	}
+	for(k=low; k<=high;k++)
+	 a[k]=b[k];
 }
 
-int main() {
-    int *arr, i;
-    clock_t start, end;
-    double time_taken;
-
-    printf("\n**MERGE SORT PROGRAM**\n");
-    printf("\nEnter the number of elements to be sorted:\n");
-    scanf("%d", &n);
-    
-    arr = (int*) malloc(n * sizeof(int));
-    if (!arr) {
-        printf("Memory allocation failed\n");
-        return 1;
-    }
-
-    srand(time(NULL));
-
-    printf("\nArray elements before sorting:\n");
-    for (i = 0; i < n; i++) {
-        arr[i] = rand() % 10000;
-        printf("%5d", arr[i]);
-    }
-
-    printf("\nArray elements to be sorted are:\n");
-    for (i = 0; i < n; i++)
-        printf("%5d", arr[i]);
-
-    start = clock();
-    mergesort(arr, 0, n - 1);
-    end = clock();
-
-    time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
-    
-    printf("\nThe sorted elements are:\n");
-    for (i = 0; i < n; i++)
-        printf("%d\t", arr[i]);
-
-    printf("\n\nThe time taken to sort %d elements is %.6f sec\n", n, time_taken);
-
-    free(arr);
-    return 0;
+void main()
+{
+	int *arr,i;
+	clock_t start,end;
+	double time_taken;
+	
+	printf("\n**MERGE SORT PROGRAM**\n");
+	printf("\nEnter the number of elements to be sorted:\n");
+	scanf("%d",&n);
+	arr=(int*)malloc(n*sizeof(int));
+	srand(time(NULL));
+	
+	printf("\nArray elements before sorting:\n");
+	for(i=0;i<n;i++)
+	{
+		arr[i]=rabd()%10000;
+		printf("%5d",arr[i]);
+	}
+	printf("\mArray elements to be sorted are\n");
+	for(i=0;i<n;i++)
+		printf("%5d",arr[i]);
+		start=clock();
+		mergesort(arr,0,n-1);
+		end=clock();
+		
+		time_taken=((double)(end-start))/CLOCKS_PER_SEC;
+		printf("\nThe sorted elements are\n");
+		for(i=0;i<n;i++)
+			printf("%d\t",arr[i]);
+			printf("\n\nThe time taken to sort %d elements is %1f sec",n,time_taken);
 }
-
